2bcbf8d7b6cf91e31c38b1c796145dc107bd8c8a
Andrey Sobchenyuk <andrey.s.itlt@gmail.com>
2018-11-22 15:31:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+?>
+
+
+<script>
+	var conn = new WebSocket('ws://localhost:8080');
+	conn.onopen = function(e) {
+		console.log("Connection established!");
+	};
+
+	conn.onmessage = function(e) {
+		console.log(e.data);
+	};
+</script>
