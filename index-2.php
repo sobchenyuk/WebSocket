4bd8f3a0be4bac380e7bcc756aad7cca7dc4b2e8
Andrey Sobchenyuk <andrey.s.itlt@gmail.com>
2018-11-22 16:29:52 +0200

    Push

---
+++ index-2.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+?>
+
+
+<script src="autobahn.js"></script>
+<script>
+	var conn = new ab.Session('ws://localhost:8080',
+		function() {
+			conn.subscribe('kittensCategory', function(topic, data) {
+				// This is where you would add the new article to the DOM (beyond the scope of this tutorial)
+				console.log('New article published to category "' + topic + '" : ' + data.title);
+			});
+		},
+		function() {
+			console.warn('WebSocket connection closed');
+		},
+		{'skipSubprotocolCheck': true}
+	);
+</script>
